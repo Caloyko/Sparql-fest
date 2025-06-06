b64f55a6bece36f5169cfc18e21b18aa40148ac6
Caloyko <pauline.lubet@gmail.com>
2025-06-06 12:59:53 +0200

    particles basic version

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import ParticlesComponent from './Particles'
 
 const HeroSection = () => {
 
         return (
             <div className="flex flex-col items-center mt-6 lg:mt-20">
+                {/*<ParticlesComponent id="particles" />*/}
+
                 <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
                     <span className="bg-gradient-to-r from-orange-500 to-red-700 text-transparent bg-clip-text">
                     {" "}Learn SPARQL 
                     </span>
-                    {" "}by example with SPARQL Fest
+                    {" "}by example <br></br>with SPARQL Fest
                 </h1>
                 <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
                     Join a collaborative project around the Semantic Web.<br></br>
