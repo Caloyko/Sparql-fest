4310a785433321dd140755f3d80df1b8d7ff4c31
Caloyko <pauline.lubet@gmail.com>
2025-06-09 17:07:58 +0200

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Section from './reusable/Section'
 import { CheckCircle2 } from 'lucide-react'
-import codeImg from "../assets/images/SPARQL-ex.png"
+import codeImg from "../assets/images/SPARQL-ex2.png"
 import { checklistItems } from "../constants";
 
 const About = () => {
@@ -16,37 +16,28 @@ const About = () => {
           <p className='text-neutral-900 mb-4'><span className='bg-gradient-to-r from-orange-500 to-red-700 text-transparent bg-clip-text'>SPARQL Fest</span> is a collaborative platform that brings together the community to learn, share, and explore SPARQL.
           It curates real SPARQL queries from people, organizations, and events to make the language easier to understand and apply.</p>
           
-        <div className="flex flex-wrap justify-center items-stretch">
-            <div className="relative p-2 w-full lg:w-1/2 flex items-center justify-center mb-24">
-              <img
-                src={codeImg}
-                alt="Coding"
-                className="absolute top-1/6  left-1/6 rounded-lg border-2 border-orange-300"
-              />      
-              <img
-                src={codeImg}
-                alt="Coding"
-                className="absolute top-1/10  left-1/5 rounded-lg border-2 border-orange-300"
-              />  
-               <img
-                src={codeImg}
-                alt="Coding"
-                className="relative z-10 rounded-lg border-2 border-orange-300"
-              />
-            </div>
-        <div className="pt-12 w-full lg:w-1/2">
-          {checklistItems.map((item, index) => (
-            <div key={index} className="flex mb-12">
-              <div className="text-green-400 mx-6 bg-neutral-100 h-10 w-10 p-2 justify-center items-center rounded-full">
-                <CheckCircle2 />
-              </div>
-              <div>
-                <h5 className="mt-1 mb-2 text-xl">{item.title}</h5>
-                <p className="text-md text-neutral-500">{item.description}</p>
-              </div>
-            </div>
-          ))}
+          <div className="flex flex-wrap justify-center items-stretch">
+  <div className="relative p-2 w-full lg:w-1/2 flex items-stretch justify-center mb-12">
+    <img
+      src={codeImg}
+      alt="Coding"
+      className="object-contain h-full w-auto max-h-full"
+    />
+  </div>
+
+  <div className="pt-12 w-full lg:w-1/2 flex flex-col justify-center">
+    {checklistItems.map((item, index) => (
+      <div key={index} className="flex mb-12">
+        <div className="text-green-400 mx-6 bg-neutral-100 h-10 w-10 p-2 justify-center items-center rounded-full">
+          <CheckCircle2 />
         </div>
+        <div>
+          <h5 className="mt-1 mb-2 text-xl">{item.title}</h5>
+          <p className="text-md text-neutral-500">{item.description}</p>
+        </div>
+      </div>
+    ))}
+  </div>
         </div>
         </Section>
     </>
