de480c878e94e2d5591e71fd8b65afe9fe6751f4
Caloyko <pauline.lubet@gmail.com>
2025-06-09 17:18:20 +0200

    landingpage review

--- src/components/WhySparql.jsx
+++ src/components/WhySparql.jsx
@@ -14,12 +14,13 @@ const WhySparql = () => {
     {features.map((feature, index) => (
         <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
             <div className="flex">
-            <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-800 text-orange-700 justify-center items-center rounded-full">
-                {feature.icon}
+            <div className="flex mx-6 h-12 w-12 bg-neutral-800 text-orange-700 justify-center items-center rounded-full aspect-square">
+            {React.cloneElement(feature.icon, { className: "w-5 h-5" })}
             </div>
+
             <div>
                 <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
-                <p className="text-md p-2 mb-20 text-neutral-500">
+                <p className="text-md mb-20 text-neutral-500 w-full text-justify">
                 {feature.description}
                 </p>
             </div>
