b5fabb1a2528d78652a2c4ff3b09fb82913234e8
Caloyko <pauline.lubet@gmail.com>
2025-06-10 11:16:17 +0200

    basic perfix page

--- src/pages/PrefixDetail.jsx
+++ src/pages/PrefixDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import InProgress from '../components/reusable/InProgress'
 import { useParams } from 'react-router-dom';
 import { sparqlPrefixes } from '../data/prefixes';
-
+import Section from '../components/reusable/Section';
 
 
 
@@ -25,7 +25,32 @@ const PrefixDetail = () => {
   console.log(slug)
 
   return (
-    <div><InProgress/>Test {detail.prefix}</div>
+    <div class="w-full px-4">
+      <div className='flex flex-col items-center mt-20'>
+      <img src={detail.logo} className='w-25 h-25' alt={detail.prefix} />
+      </div>
+
+    <Section
+        id={detail.prefix}
+        title={detail.name}
+        description={detail.prefix}
+        variant="dark"
+        bg={false}
+        >
+            <h2 class="text-xl font-bold mt-4 mb-2">Description</h2>
+            <p class="text-neutral-300 mb-6">{detail.description}</p>
+            <p class="text-neutral-400 mb-6">{detail.version}</p>
+            <p class="text-neutral-400 mb-6">{detail.category}</p>
+            <h2 class="text-xl font-bold mt-4 mb-2">documentation</h2>
+            <p class="text-neutral-300 mb-6">{detail.documentation}</p>
+            <h2 class="text-md font-bold mt-4 mb-2">download</h2>
+            <p class="text-neutral-300 mb-6">{detail.download}</p>
+            <h2 class="text-md font-bold mt-4 mb-2">version</h2>
+            <p class="text-neutral-300 mb-6">{detail.version}</p>
+            
+    </Section>
+       
+    </div>
   )
 }
 
